c65b568835bf6873e7e33070887aa64bf6d60324
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2022-03-31 13:41:13 +0530

    CodeFear

---
+++ Section1/src/FirstDataType.java
@@ -0,0 +1,7 @@
+public class FirstDataType {
+    public static void main(String[] args) {
+        String myVar ="what's the day?";
+
+        System.out.println(myVar);
+    }
+}
---
+++ Section1/src/FirstMethod.java
@@ -0,0 +1,14 @@
+public class FirstMethod {
+    public static void main(String[] args) {
+        //main method
+        //Method are basically instructions
+        printHello();
+        printHello();
+        //calling the "printHello();" method
+    }
+
+    //another method
+    static void printHello(){
+        System.out.println("here we go....");
+
+}}
