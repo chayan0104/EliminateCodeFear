b9ec435192fd24ac9c5459f788cdcac6fe66e707
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2022-03-31 15:41:10 +0530

    CodeFear

---
+++ Section1/src/FirstReturn.java
@@ -0,0 +1,13 @@
+public class FirstReturn {
+    public static void main(String[] args) {
+
+        int myVar = sumthese(24,25);
+        System.out.println(myVar);
+
+
+    }
+    static int sumthese( int i,int j) {
+        return i+j;
+
+    }
+}
